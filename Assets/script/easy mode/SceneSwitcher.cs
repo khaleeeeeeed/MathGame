e6f3a263f90cc9296e8db1a35737aa5de13e4b22
khaleeeeeeed <105913035+khaleeeeeeed@users.noreply.github.com>
2022-05-20 16:53:45 +0700

    initial commit

---
+++ Assets/script/easy mode/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitcher : MonoBehaviour
+{
+    public void nextGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+        
+    
+}
---
+++ Assets/script/hard mode/SlotScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SlotScript : MonoBehaviour, IDropHandler
+{
+    public int id;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log("Item Dropped");
+        if(eventData.pointerDrag != null)
+        {
+            if (eventData.pointerDrag.GetComponent<DragAndDrop>().id == id)
+            {
+                Debug.Log("Correct!");
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+            }
+            else
+            {
+                Debug.Log("Wrong!");
+                eventData.pointerDrag.GetComponent<DragAndDrop>().ResetPosition();
+            }
+        }
+    }
+ 
+}
